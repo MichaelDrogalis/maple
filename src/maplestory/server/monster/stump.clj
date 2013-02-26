9fd8f6a0ccf2248600a09354cfdfc94426bb00a6
Michael Drogalis <mjd3089@rit.edu>
2013-02-26 14:50:46 -0500

    Trying to refactor stump movement.

--- src/maplestory/server/monster/stump.clj
+++ src/maplestory/server/monster/stump.clj
@@ -1,16 +1,42 @@
 (ns maplestory.server.monster.stump
   (:require [maplestory.server.movement :refer [can-move-right? can-move-left? stand! flip! flip]]))
 
-(def spawn-state {:type :stump :origin {:x 650 :y 500} :x-step 20
-                  :direction :left :x-span 100 :action :walk})
+(def spawn-state {:type :stump
+                  :direction :left
+                  :step 20
+                  :origin {:x 650 :y 500}
+                  :boundaries {:x {:left 550 :right 750}}
+                  :action :walk})
 
-(defn walk [state]
+(defn should-turn-around? [{:keys [x]} direction boundaries]
+  (let [{:keys [left right]} (:x boundaries)]
+    (if (= direction :right)
+      (>= x right)
+      (<= x left))))
+
+(defn units-in-direction [{:keys [x]} step-length direction boundaries]
+  (if (= direction :right)
+    (if (>= step-length (- (:right (:x boundaries)) x))
+      step-length
+      (- (:right (:x boundaries)) x))
+    (if (>= step-length (- x (:left (:x boundaries))))
+      (* -1 step-length)
+      (* -1 (- x (:left (:x boundaries)))))))
+
+(defn move! [state]
   (send state
-        (fn [{:keys [x x-span x-step origin direction] :as monster}]
-          (cond (can-move-left? (:x origin) x x-span direction) (assoc monster :action :walk :x (- x x-step))
-                (can-move-right? (:x origin) x x-span direction) (assoc monster :action :walk :x (+ x x-step))
-                :else (flip monster))))
+        (fn [{:keys [position step direction boundaries] :as monster}]
+          (assoc-in monster
+                    [:action] :walk
+                    [:position :x] (+ (:x position)
+                                      (units-in-direction position step direction boundaries)))))
   (Thread/sleep 1000))
 
-(def actions [walk])
+(defn move [state]
+  (let [{:keys [position direction boundaries]} @state]
+    (if (should-turn-around? position direction boundaries)
+      (flip! state)
+      (move! state))))
+
+(def actions [move])
 
