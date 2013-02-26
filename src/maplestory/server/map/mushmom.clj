ba4eb3a318e4249311fe6fb565c00018f1a0aabf
Michael Drogalis <mjd3089@rit.edu>
2013-02-26 13:36:05 -0500

    Some more work on footing.

--- src/maplestory/server/map/mushmom.clj
+++ src/maplestory/server/map/mushmom.clj
@@ -1,10 +1,16 @@
 (ns maplestory.server.map.mushmom
-  (:require [maplestory.server.monster.stump :as stump]
-            [maplestory.server.movement :refer [scheduler]]
+  (:require [clojure.set :refer [union]]
+            [maplestory.server.monster.stump :as stump]
+            [maplestory.server.movement :refer [scheduler platform]]
             [maplestory.server.map.binders :refer [register-map! spawn birth]]))
 
 (def specs {:width  1176
-            :height 1154})
+            :height 1154
+            :footing (union (platform :from 249 :to 562  :on 240)
+                            (platform :from 612 :to 742  :on 301)
+                            (platform :from 165 :to 650  :on 423)
+                            (platform :from 72  :to 473  :on 600)
+                            (platform :from 72  :to 1108 :on 871))})
 
 (let [map-name :mushmom]
   (register-map! map-name)
--- src/maplestory/server/movement.clj
+++ src/maplestory/server/movement.clj
@@ -23,6 +23,25 @@
 (defn flip! [state]
   (send state flip))
 
+(defn advance-horizontally [{:keys [origin x-step footing]}])
+
+#_(defn move! [state]
+  (let [x-motion (advance-horizontally @state)]
+    (send state walk)
+    (when (< x-motion x-step)
+      (send state fall))))
+
+#_(defn move [state]
+  (let [{:keys [max-left max-right origin direction x-step]} @state]
+    (cond (should-turn-around?) (flip! state)
+          (move! state)))
+  (send state
+        (fn [{:keys [x x-span x-step origin direction] :as monster}]
+          (cond (can-move-left? (:x origin) x x-span direction) (assoc monster :action :walk :x (- x x-step))
+                (can-move-right? (:x origin) x x-span direction) (assoc monster :action :walk :x (+ x x-step))
+                :else (flip monster))))
+  (Thread/sleep 1000))
+
 (defn scheduler [agent actions]
   (doseq [action (shuffle actions)]
     (if (sequential? action)
@@ -30,4 +49,7 @@
         (subaction agent))
       (action agent)))
   (recur agent actions))
+
+(defn platform [& {:keys [from to on]}]
+  (reduce #(conj %1 {:x %2 :y on}) #{} (range from (inc to))))
 
