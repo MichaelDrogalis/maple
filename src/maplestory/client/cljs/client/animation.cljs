1f655f4329a8835efaddcfae5f25ed157d1b58da
Michael Drogalis <mjd3089@rit.edu>
2013-02-27 09:39:02 -0500

    Cljs refactoring.

--- src/maplestory/client/cljs/client/animation.cljs
+++ src/maplestory/client/cljs/client/animation.cljs
@@ -1,16 +1,28 @@
-(ns client.animation)
-
-(def jquery (js* "$"))
+(ns client.animation
+  (:require [client.jquery :refer [jq jq-id]]))
 
 (defn swap-image! [selector duration classes]
   (let [queue selector]
     (doseq [class classes]
-      (.queue (.delay (jquery (str "#" selector)) duration queue)
+      (.queue (.delay (jq-id selector) duration queue)
               queue
               (fn [next-fn]
-                (if (.hasClass (jquery (str "#" selector)) "mirrored")
-                  (.attr (jquery (str "#" selector)) "class" (str "mirrored" " " class))
-                  (.attr (jquery (str "#" selector)) "class" class))
+                (if (.hasClass (jq-id selector) "mirrored")
+                  (.attr (jq-id selector) "class" (str "mirrored" " " class))
+                  (.attr (jq-id selector) "class" class))
                 (next-fn))))
-    (.dequeue (jquery (str "#" selector)) queue)))
+    (.dequeue (jq-id selector) queue)))
+
+(defn init [{:keys [id position direction]} container offset-height]
+  (.append (jq "body") (container id))
+  (.css (jq-id id) "left" (:x position))
+  (.css (jq-id id) "top" (- (:y position) offset-height))
+  (if (= direction :right)
+    (.addClass (jq-id id) "mirrored"))
+  (.css (jq-id id) "visibility" "visible"))
+
+(defn flip [{:keys [id direction]}]
+  (if (= direction :left)
+    (.removeClass (jq-id id) "mirrored")
+    (.addClass (jq-id id) "mirrored")))
 
--- src/maplestory/client/cljs/client/monster/stump.cljs
+++ src/maplestory/client/cljs/client/monster/stump.cljs
@@ -2,35 +2,25 @@
   (:require-macros [hiccups.core :refer [defhtml]])
   (:require [hiccups.runtime :as hrt]
             [client.jquery :refer [jq jq-id]]
-            [client.animation :refer [swap-image!]]))
+            [client.animation :as animate]))
 
 (defhtml stump-container [id]
   [:div {:type :stump :id id}])
 
-(defn offset-height [y]
-  (- y 40))
-
-(defn init [{:keys [id position direction]}]
-  (.append (jq "body") (stump-container id))
-  (.css (jq-id id) "left" (:x position))
-  (.css (jq-id id) "top" (offset-height (:y position)))
-  (if (= direction :right)
-    (.addClass (jq-id id) "mirrored"))
-  (.css (jq-id id) "visibility" "visible"))
+(defn init [monster]
+  (animate/init monster stump-container 40))
 
 (defmulti update :action)
 
-(defmethod update :flip [{:keys [id direction]}]
-  (if (= direction :left)
-    (.removeClass (jq-id id) "mirrored")
-    (.addClass (jq-id id) "mirrored")))
+(defmethod update :flip [monster]
+  (animate/flip monster))
 
 (defmethod update :stand [{:keys [id]}]
-  (swap-image! id 2000 ["stump-stand0"]))
+  (animate/swap-image! id 2000 ["stump-stand0"]))
 
 (defmethod update :walk [{:keys [id position]}]
-  (swap-image! id 250 ["stump-move0" "stump-move1" "stump-move2" "stump-move3"])
-  (.animate (jq-id id) (clj->js {:left (:x position) :top (offset-height (:y position))}) 1000))
+  (animate/swap-image! id 250 ["stump-move0" "stump-move1" "stump-move2" "stump-move3"])
+  (.animate (jq-id id) (clj->js {:left (:x position) :top (- (:y position) 40)}) 1000))
 
 (defmethod update :default [])
 
